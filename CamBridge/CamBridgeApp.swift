4d13df944243995c3a35b1c41fa22944dd827c55
Mikhail Safir <safir@thenoco.co>
2025-03-25 00:29:04 +0100

    Initial Commit

---
+++ CamBridge/CamBridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CamBridgeApp.swift
+//  CamBridge
+//
+//  Created by Michael Safir on 25/3/25.
+//
+
+import SwiftUI
+
+@main
+struct CamBridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
